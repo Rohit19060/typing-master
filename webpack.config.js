82e49d7d0d1900c11607b7f26ec257a1be82557a
Rohit19060 <rohitjain19060@gmail.com>
2021-04-22 15:01:09 +0530

    Updated Directory structure with bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'assets/scripts/'),
+        path: path.resolve(__dirname, ''),
     },
     module: {
         rules: [
